d45597446d2b4b4ad01ceb1d9c26cace8436b2c8
Michael HIllcox <michaelhillcox@gmail.com>
2021-06-15 15:44:36 +0100

    feat: support for higher cpt

--- src/main/java/pro/mikey/autoclicker/OptionsScreen.java
+++ src/main/java/pro/mikey/autoclicker/OptionsScreen.java
@@ -6,122 +6,122 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 
 public class OptionsScreen extends Screen {
-  protected OptionsScreen() {
-    super(LiteralText.EMPTY);
-  }
-
-  @Override
-  protected void init() {
-    int x = this.width / 2, y = this.height / 2;
-
-    this.addButton(
-        new ButtonWidget(
-            x - 135,
-            y - 44,
-            130,
-            20,
-            Language.GUI_ACTIVE.getText(AutoClicker.leftHolding.isActive()),
-            button -> {
-              AutoClicker.leftHolding.setActive(!AutoClicker.leftHolding.isActive());
-              button.setMessage(Language.GUI_ACTIVE.getText(AutoClicker.leftHolding.isActive()));
-            }));
-    this.addButton(
-        new ButtonWidget(
-            x + 5,
-            y - 44,
-            130,
-            20,
-            Language.GUI_ACTIVE.getText(AutoClicker.rightHolding.isActive()),
-            button -> {
-              AutoClicker.rightHolding.setActive(!AutoClicker.rightHolding.isActive());
-              button.setMessage(Language.GUI_ACTIVE.getText(AutoClicker.rightHolding.isActive()));
-            }));
-    this.addButton(
-        new ButtonWidget(
-            x - 135,
-            y - 22,
-            130,
-            20,
-            Language.GUI_SPAMMING.getText(AutoClicker.leftHolding.isSpamming()),
-            button -> {
-              AutoClicker.leftHolding.setSpamming(!AutoClicker.leftHolding.isSpamming());
-              button.setMessage(
-                  Language.GUI_SPAMMING.getText(AutoClicker.leftHolding.isSpamming()));
-            }));
-    this.addButton(
-        new ButtonWidget(
-            x + 5,
-            y - 22,
-            130,
-            20,
-            Language.GUI_SPAMMING.getText(AutoClicker.rightHolding.isSpamming()),
-            button -> {
-              AutoClicker.rightHolding.setSpamming(!AutoClicker.rightHolding.isSpamming());
-              button.setMessage(
-                  Language.GUI_SPAMMING.getText(AutoClicker.rightHolding.isSpamming()));
-            }));
-    this.addButton(
-        new OptionsSliderWidget(
-            x - 135,
-            y,
-            130,
-            20,
-            Language.GUI_SPEED.getText(),
-            AutoClicker.leftHolding.getSpeed() / 10f,
-            value -> AutoClicker.leftHolding.setSpeed(value)));
-    this.addButton(
-        new OptionsSliderWidget(
-            x + 5,
-            y,
-            130,
-            20,
-            Language.GUI_SPEED.getText(),
-            AutoClicker.rightHolding.getSpeed() / 10f,
-            value -> AutoClicker.rightHolding.setSpeed(value)));
-    this.addButton(
-        new ButtonWidget(
-            x - 135,
-            y + 22,
-            130,
-            20,
-            Language.GUI_RESPECT_COOLDOWN.getText(AutoClicker.leftHolding.isRespectCooldown()),
-            button -> {
-              AutoClicker.leftHolding.setRespectCooldown(
-                  !AutoClicker.leftHolding.isRespectCooldown());
-              button.setMessage(
-                  Language.GUI_RESPECT_COOLDOWN.getText(
-                      AutoClicker.leftHolding.isRespectCooldown()));
-            }));
-  }
-
-  @Override
-  public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-    this.renderBackground(matrices);
-    super.render(matrices, mouseX, mouseY, delta);
-
-    textRenderer.drawWithShadow(
-        matrices,
-        Language.GUI_ATTACK.getText(),
-        this.width / 2f - 135,
-        this.height / 2f - 56,
-        0xFFFFFF);
-
-    textRenderer.drawWithShadow(
-        matrices, Language.GUI_USE.getText(), this.width / 2f + 5, this.height / 2f - 56, 0xFFFFFF);
-  }
-
-  @Override
-  public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-    if (keyCode == AutoClicker.rightClickToggle.getDefaultKey().getCode()) {
-      this.onClose();
-      return true;
+    protected OptionsScreen() {
+        super(LiteralText.EMPTY);
     }
 
-    return super.keyPressed(keyCode, scanCode, modifiers);
-  }
+    @Override
+    protected void init() {
+        int x = this.width / 2, y = this.height / 2;
 
-  @Override
-  public boolean isPauseScreen() {
-    return false;
-  }
+        this.addButton(
+                new ButtonWidget(
+                        x - 135,
+                        y - 44,
+                        130,
+                        20,
+                        Language.GUI_ACTIVE.getText(AutoClicker.leftHolding.isActive()),
+                        button -> {
+                            AutoClicker.leftHolding.setActive(!AutoClicker.leftHolding.isActive());
+                            button.setMessage(Language.GUI_ACTIVE.getText(AutoClicker.leftHolding.isActive()));
+                        }));
+        this.addButton(
+                new ButtonWidget(
+                        x + 5,
+                        y - 44,
+                        130,
+                        20,
+                        Language.GUI_ACTIVE.getText(AutoClicker.rightHolding.isActive()),
+                        button -> {
+                            AutoClicker.rightHolding.setActive(!AutoClicker.rightHolding.isActive());
+                            button.setMessage(Language.GUI_ACTIVE.getText(AutoClicker.rightHolding.isActive()));
+                        }));
+        this.addButton(
+                new ButtonWidget(
+                        x - 135,
+                        y - 22,
+                        130,
+                        20,
+                        Language.GUI_SPAMMING.getText(AutoClicker.leftHolding.isSpamming()),
+                        button -> {
+                            AutoClicker.leftHolding.setSpamming(!AutoClicker.leftHolding.isSpamming());
+                            button.setMessage(
+                                    Language.GUI_SPAMMING.getText(AutoClicker.leftHolding.isSpamming()));
+                        }));
+        this.addButton(
+                new ButtonWidget(
+                        x + 5,
+                        y - 22,
+                        130,
+                        20,
+                        Language.GUI_SPAMMING.getText(AutoClicker.rightHolding.isSpamming()),
+                        button -> {
+                            AutoClicker.rightHolding.setSpamming(!AutoClicker.rightHolding.isSpamming());
+                            button.setMessage(
+                                    Language.GUI_SPAMMING.getText(AutoClicker.rightHolding.isSpamming()));
+                        }));
+        this.addButton(
+                new OptionsSliderWidget(
+                        x - 135,
+                        y,
+                        130,
+                        20,
+                        Language.GUI_SPEED.getText(),
+                        AutoClicker.leftHolding.getSpeed() / 50f,
+                        value -> AutoClicker.leftHolding.setSpeed(value)));
+        this.addButton(
+                new OptionsSliderWidget(
+                        x + 5,
+                        y,
+                        130,
+                        20,
+                        Language.GUI_SPEED.getText(),
+                        AutoClicker.rightHolding.getSpeed() / 50f,
+                        value -> AutoClicker.rightHolding.setSpeed(value)));
+        this.addButton(
+                new ButtonWidget(
+                        x - 135,
+                        y + 22,
+                        130,
+                        20,
+                        Language.GUI_RESPECT_COOLDOWN.getText(AutoClicker.leftHolding.isRespectCooldown()),
+                        button -> {
+                            AutoClicker.leftHolding.setRespectCooldown(
+                                    !AutoClicker.leftHolding.isRespectCooldown());
+                            button.setMessage(
+                                    Language.GUI_RESPECT_COOLDOWN.getText(
+                                            AutoClicker.leftHolding.isRespectCooldown()));
+                        }));
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        super.render(matrices, mouseX, mouseY, delta);
+
+        this.textRenderer.drawWithShadow(
+                matrices,
+                Language.GUI_ATTACK.getText(),
+                this.width / 2f - 135,
+                this.height / 2f - 56,
+                0xFFFFFF);
+
+        this.textRenderer.drawWithShadow(
+                matrices, Language.GUI_USE.getText(), this.width / 2f + 5, this.height / 2f - 56, 0xFFFFFF);
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (keyCode == AutoClicker.rightClickToggle.getDefaultKey().getCode()) {
+            this.onClose();
+            return true;
+        }
+
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
+    }
 }
--- src/main/java/pro/mikey/autoclicker/OptionsSliderWidget.java
+++ src/main/java/pro/mikey/autoclicker/OptionsSliderWidget.java
@@ -6,22 +6,21 @@ import net.minecraft.text.Text;
 import java.util.function.Consumer;
 
 public class OptionsSliderWidget extends SliderWidget {
-  public Consumer<Integer> onUpdate;
+    public Consumer<Integer> onUpdate;
 
-  public OptionsSliderWidget(
-      int x, int y, int width, int height, Text text, double value, Consumer<Integer> onUpdate) {
-    super(x, y, width, height, text, value);
-    this.onUpdate = onUpdate;
-    this.updateMessage();
-  }
+    public OptionsSliderWidget(int x, int y, int width, int height, Text text, double value, Consumer<Integer> onUpdate) {
+        super(x, y, width, height, text, value);
+        this.onUpdate = onUpdate;
+        this.updateMessage();
+    }
 
-  @Override
-  protected void updateMessage() {
-    this.setMessage(Language.GUI_SPEED.getText((int) (this.value * 10)));
-  }
+    @Override
+    protected void updateMessage() {
+        this.setMessage(Language.GUI_SPEED.getText((int) (this.value * 50)));
+    }
 
-  @Override
-  protected void applyValue() {
-    this.onUpdate.accept((int) (this.value * 10));
-  }
+    @Override
+    protected void applyValue() {
+        this.onUpdate.accept((int) (this.value * 50));
+    }
 }
