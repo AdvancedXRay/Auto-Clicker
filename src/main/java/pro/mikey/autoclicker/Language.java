a6303f1c9af0805e67a8b939cbc5788e1c8841d2
Andrey "Zed" Zaikin <andrey.zaikin@leadfeeder.com>
2022-07-16 16:07:40 +0300

    cleanup

--- src/main/java/pro/mikey/autoclicker/Language.java
+++ src/main/java/pro/mikey/autoclicker/Language.java
@@ -6,16 +6,7 @@ import net.minecraft.text.Text;
 public enum Language {
     HUD_HOLDING("autoclicker-fabric.hud.holding"),
     MSG_HOLDING_KEYS("autoclicker-fabric.msg.holding-keys"),
-    MSG_RELEASED_KEYS("autoclicker-fabric.msg.released-keys"),
-    GUI_SPEED("autoclicker-fabric.gui.speed"),
-    GUI_ACTIVE("autoclicker-fabric.gui.active"),
-    GUI_SPAMMING("autoclicker-fabric.gui.spamming"),
-    GUI_ATTACK("autoclicker-fabric.gui.attack"),
-    GUI_USE("autoclicker-fabric.gui.use"),
-    GUI_RESPECT_COOLDOWN("autoclicker-fabric.gui.respect"),
-    GUI_MOB_MODE("autoclicker-fabric.gui.mob-mode"),
-    GUI_CROP_MODE("autoclicker-fabric.gui.crop-mode"),
-    GUI_INERTIA("autoclicker-fabric.gui.inertia");
+    MSG_RELEASED_KEYS("autoclicker-fabric.msg.released-keys");
 
     private final String key;
     MutableText text;
