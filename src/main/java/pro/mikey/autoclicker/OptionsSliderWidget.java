4f908add55de6c575867c98edef9923a0cd3ffa1
Joseph Molloy <joseph.molloy.ctr@progeny.net>
2023-02-08 09:43:14 -0500

    round slider values to prevent one off dsiplayed value from int cast truncation

--- src/main/java/pro/mikey/autoclicker/OptionsSliderWidget.java
+++ src/main/java/pro/mikey/autoclicker/OptionsSliderWidget.java
@@ -16,11 +16,11 @@ public class OptionsSliderWidget extends SliderWidget {
 
     @Override
     protected void updateMessage() {
-        this.setMessage(Language.GUI_SPEED.getText((int) (this.value * 50)));
+        this.setMessage(Language.GUI_SPEED.getText((int) Math.round(this.value * 50)));
     }
 
     @Override
     protected void applyValue() {
-        this.onUpdate.accept((int) (this.value * 50));
+        this.onUpdate.accept((int) Math.round(this.value * 50));
     }
 }
